f380a62323fe2ccad07a7c66975f85362d2681f0
Shod <dilshod.sadiev@outlook.com>
2019-07-25 22:25:20 -0400

    ver 2

--- SimpleScanning/SimpleScanning/Navigation.cs
+++ SimpleScanning/SimpleScanning/Navigation.cs
@@ -1,60 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace SimpleScanning
 {
     class Navigation
     {
+        int page;//Current page
+        const int ItemsPerPage = 5;//Tasks per page
         public void KeyCatch(List<MyTask> taskList)
         {
-            TaskClass taskClass = new TaskClass();//need it to get access to save and open XML
+            XMLFile file = new XMLFile(); //needs it to save or open tasks in XML file
+            int index = 0; //current task
+            int cursor = 0;
 
-            PrintTasks(0, taskList);
+            PrintTasks(index, taskList);
             ConsoleKeyInfo consoleKeyInfo;
             Console.SetCursorPosition(0, 0);
-            int index = 0;
-
-            if (index <= taskList.Count)
-            {
-                if (taskList[index].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.Write(taskList[index].TaskName.PadRight(120, ' '));
-            }
-
-            while ((consoleKeyInfo = Console.ReadKey()).Key != ConsoleKey.F12)
+            
+            if (index <= taskList.Count) SelectTask(index, taskList);//select current task first time
+            
+            while ((consoleKeyInfo = Console.ReadKey()).Key != ConsoleKey.F12) //if pressed F12 close app
             {
                 switch (consoleKeyInfo.Key)
                 {
                     case ConsoleKey.F1://skip
                         index++;
-                        SkipTask(ref index, taskList);
+                        cursor++;
+                        MoveToNext(ref index, ref cursor, taskList);
                         break;
                     case ConsoleKey.F2://done
-                        done(index, taskList);
-                        taskClass.SaveTasks(taskList);
+                        taskList[index].Status = 1;//cross-out current task
+                        file.Save(taskList);
                         index++;
-                        SkipTask(ref index, taskList);
+                        cursor++;
+                        MoveToNext(ref index, ref cursor, taskList);
                         break;
                     case ConsoleKey.F3://Re-write
-                        if (taskList[index].Status != 1)
+                        if (taskList[index].Status != 1)//if active task then re-write
                         {
-                            done(index, taskList);
-                            RewriteTask(index, taskList);
-                            taskClass.SaveTasks(taskList);
-                            PrintTasks(index, taskList);
+                            taskList[index].Status = 1;
+                            taskList.Add(new MyTask(0, taskList[index].TaskName, DateTime.Now));//RewriteTask
+                            file.Save(taskList);
+                            PrintTasks(page, taskList);
                         }
+                        cursor++;
                         index++;
-                        SkipTask(ref index, taskList);
+                        MoveToNext(ref index, ref cursor, taskList);
                         break;
                     case ConsoleKey.F4://add a new task
                         AddTask(taskList);
-                        taskClass.SaveTasks(taskList);
-                        PrintTasks(index, taskList);
-                        Console.SetCursorPosition(0, index);
-                        if (taskList[index].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.Write(taskList[index].TaskName.PadRight(120, ' '));
+                        file.Save(taskList);
+                        PrintTasks(page, taskList);
+                        Console.SetCursorPosition(0, cursor);
+                        SelectTask(index, taskList);
+                        break;
+                    case ConsoleKey.PageDown://next page
+                        cursor = ItemsPerPage;
+                        index = page + ItemsPerPage;
+                        MoveToNext(ref index, ref cursor, taskList);
                         break;
                     default:
                         
@@ -64,67 +68,65 @@ namespace SimpleScanning
             clearConsol();//Clear console before closing app
         }
 
-        public void SkipTask(ref int index, List<MyTask> taskList)
+        void SelectTask(int index, List<MyTask> taskList)//This method select current task with a green line 
+        {
+            if (taskList[index].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(taskList[index].TaskName.PadRight(120, ' '));
+        }
+
+        void PrintTasks(int index, List<MyTask> taskList) //This method print -ItemsPerPage- and Menu
+        {
+            clearConsol();
+            page = index;
+
+            foreach (var item in taskList.Skip(index).Take(ItemsPerPage))
+            {
+                Console.ResetColor();
+                Console.ForegroundColor = item.Status == 1 ? ConsoleColor.Red : ConsoleColor.Gray;
+                Console.WriteLine(item.TaskName);
+            }
+            //Print menu with blue color
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(0, ItemsPerPage + 1);
+            DisplayMenu();
+        }
+        void MoveToNext(ref int index, ref int cursor, List<MyTask> taskList)
         {
             if (index >= 0 && index < taskList.Count)
             {
-                Console.SetCursorPosition(0, index - 1);
+                Console.SetCursorPosition(0, cursor - 1);
                 Console.ResetColor();
                 if (taskList[index - 1].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write(taskList[index - 1].TaskName.PadRight(120, ' '));
-                Console.SetCursorPosition(0, index);
+                //if a last task on the page than print next page
+                if (Console.CursorTop == ItemsPerPage-1)
+                {
+                    PrintTasks(index, taskList);
+                    cursor = 0;
+                } 
+                Console.SetCursorPosition(0, cursor);
                 Console.ResetColor();
                 Console.BackgroundColor = ConsoleColor.Green;
                 if (taskList[index].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write(taskList[index].TaskName.PadRight(120, ' '));
             }
-            else if (index >= taskList.Count)
+            else if (index >= taskList.Count)//move to begin of the list
             {
-                Console.SetCursorPosition(0, index - 1);
-                Console.ResetColor();
-                if (taskList[index - 1].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(taskList[index - 1].TaskName.PadRight(120, ' '));
                 index = 0;
+                PrintTasks(index, taskList);
+                cursor = 0;
                 Console.SetCursorPosition(0, 0);
-                if (taskList[index].Status == 1) Console.ForegroundColor = ConsoleColor.Red;
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.Write(taskList[index].TaskName.PadRight(120, ' '));
+                SelectTask(index, taskList);
             }
         }
-
-        public void done(int index, List<MyTask> taskList)
-        {
-            taskList[index].Status = 1;
-        }
-
-        public void RewriteTask(int index, List<MyTask> taskList)
-        {
-            taskList.Add(new MyTask(0, taskList[index].TaskName, DateTime.Now));
-            clearConsol();
-        }
-
-        public void PrintTasks(int index, List<MyTask> taskList)
-        {
-            Console.Clear();
-            for (int i = 0; i < taskList.Count; i++)
-            {
-                if (i >= taskList.Count) break;
-                Console.SetCursorPosition(0, i);
-                Console.ResetColor();
-                Console.ForegroundColor = taskList[i].Status == 1 ? ConsoleColor.Red : ConsoleColor.Gray;
-                Console.Write(taskList[i].TaskName);
-            }
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.SetCursorPosition(0, taskList.Count + 1);
-            DisplayMenu();
-        }
+        
         public void AddTask(List<MyTask> taskList)
         {
             clearConsol();
             Console.WriteLine("Enter a new task > ");
             taskList.Add(new MyTask(0, Console.ReadLine(), DateTime.Now));
-            clearConsol();
         }
 
         void clearConsol()
@@ -134,13 +136,14 @@ namespace SimpleScanning
             Console.Clear();
         }
 
-        public void DisplayMenu() //Menu
+        void DisplayMenu() //Menu
         {
             Console.WriteLine(new string('=', 120));
-            Console.Write(" F1. Skip");
+            Console.Write(" F1. Skipp");
             Console.Write(" || F2. Cross out");
             Console.Write(" || F3. Re-enter");
             Console.Write(" || F4. Add Task");
+            Console.Write(" || PgDn. Next Page");
             Console.WriteLine(" || F12. Exit");
             Console.WriteLine(new string('=', 120));
         }
--- SimpleScanning/SimpleScanning/Program.cs
+++ SimpleScanning/SimpleScanning/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Xml.Serialization;
 
 // Progect - Simple Scanning
@@ -12,57 +11,49 @@ namespace SimpleScanning
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            TaskClass taskClass = new TaskClass();
-            Navigation nav = new Navigation();
-          
-            taskClass.OpenTasks(); //Upload tasks from XML file
-            if (taskClass.TaskList.Count<=0) //if the XML file is empty then ask user input a new task
+            XMLFile file = new XMLFile();
+            Navigation nav = new Navigation();      
+            List<MyTask> list = new List<MyTask>();
+
+            file.Open(ref list); //Upload tasks from XML file
+            if (list.Count <= 0) //if the XML file is empty then ask user input a new task
             {
-                nav.AddTask(taskClass.TaskList);
-                taskClass.SaveTasks(taskClass.TaskList);
-            }      
-            nav.KeyCatch(taskClass.TaskList);  //Show the list of the tasks       
+                nav.AddTask(list);
+                file.Save(list);
+            }
+            nav.KeyCatch(list);
         }
     }
 
-    [XmlRoot("ArrayOfMyTask")]
-    class TaskClass
+    class XMLFile
     {
-        List<MyTask> taskList = new List<MyTask>();
-
-        [XmlElement("MyTask")]
-        public List<MyTask> TaskList
-        {
-            get { return taskList; }
-            set { taskList = value; }
-        }
-      
-        public void SaveTasks(List<MyTask> t)
+        public void Save(List<MyTask> t)
         {
             XmlSerializer serializer = new XmlSerializer(t.GetType());
             using (StreamWriter writer = new StreamWriter(@"tasks.xml"))
             {
                 serializer.Serialize(writer, t);
                 //Console.WriteLine("Объект был сериализирован");
-            }          
+            }
         }
 
-        public void OpenTasks()
+        public void Open(ref List<MyTask> t)
         {
-            XmlSerializer serializer = new XmlSerializer(taskList.GetType());
+            XmlSerializer serializer = new XmlSerializer(t.GetType());
             try
             {
                 using (StreamReader reader = new StreamReader(@"tasks.xml"))
                 {
-                    TaskList = (List<MyTask>)serializer.Deserialize(reader);
+                    t = (List<MyTask>)serializer.Deserialize(reader);
                 }
             }
             catch (Exception e)// if the file doesn't exist then create one
             {
-                //Console.WriteLine(e.Message);
-                SaveTasks(TaskList);
+                Console.WriteLine(e.Message);
+                Save(t);
             }
         }
     }
